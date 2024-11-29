50871740609992d76821543f57f521589ae3d33e
asnirahma <asnirahmayanti34@gmail.com>
2024-11-29 07:21:17 +0700

    Menggunakan bootstrap 5

--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,24 @@
 <?php
 
 use App\Http\Controllers\RoomController;
-use App\Http\Controllers\PatientController;
 use App\Http\Controllers\LevelController;
 use Illuminate\Support\Facades\Route;
 
+// Route untuk home page
 Route::get('/', function () {
   $mahasiswa = "Asni";
   $umur = 20;
-  return view('index')->with('m', $mahasiswa);
+  return view('index', compact('mahasiswa', 'umur'));
 });
 
-Route::get('/', function () {
-  return view('index');
-});
+// Route untuk rooms
+Route::get('/rooms', [RoomController::class, 'index'])->name('rooms.index'); // Mengarah ke index method RoomController
+Route::get('/rooms/{room}', [RoomController::class, 'show']); // Mengarah ke show method RoomController
+Route::get('/rooms/{room}/edit', [RoomController::class, 'edit'])->name('rooms.edit'); // Mengarah ke edit method RoomController
+Route::put('/rooms/{room}', [RoomController::class, 'update'])->name('rooms.update'); // Mengarah ke update method RoomController
+Route::get('/rooms/create', [RoomController::class, 'create'])->name('rooms.create'); // Mengarah ke create method RoomController
+Route::post('/rooms', [RoomController::class, 'store'])->name('rooms.store'); // Mengarah ke store method RoomController
 
-Route::get('/rooms', [RoomController::class, 'index']);
-Route::get('/rooms/{room}', [RoomController::class, 'show']);
-Route::get('/rooms/{room}/edit', [RoomController::class, 'edit'])->name('rooms.edit');
-Route::put('/rooms/{room}', [RoomController::class, 'update'])->name('rooms.update');
-Route::get('/rooms/create', [RoomController::class, 'create'])->name('rooms.create');
-Route::post('/rooms', [RoomController::class, 'store'])->name('rooms.store');
-Route::get('/levels/create', [LevelController::class, 'create'])->name('levels.create');
-Route::post('/levels', [LevelController::class, 'store'])->name('levels.store');
-Route::get('/rooms', [RoomController::class, 'index'])->name('rooms.index');
-Route::get('/rooms/{id}/edit', [RoomController::class, 'edit'])->name('rooms.edit');
-Route::put('/rooms/{id}', [RoomController::class, 'update'])->name('rooms.update');
+// Route untuk levels
+Route::get('/levels/create', [LevelController::class, 'create'])->name('levels.create'); // Untuk membuat level baru
+Route::post('/levels', [LevelController::class, 'store'])->name('levels.store'); // Untuk menyimpan level baru
